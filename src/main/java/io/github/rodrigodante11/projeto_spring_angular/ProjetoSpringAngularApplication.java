d9737fb230321691ec56e773809e73e2b100ec20
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2023-08-14 11:51:59 -0300

    Comecando o projeto

---
+++ src/main/java/io/github/rodrigodante11/projeto_spring_angular/ProjetoSpringAngularApplication.java
@@ -0,0 +1,13 @@
+package io.github.rodrigodante11.projeto_spring_angular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringAngularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoSpringAngularApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/rodrigodante11/projeto_spring_angular/ProjetoSpringAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.rodrigodante11.projeto_spring_angular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringAngularApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
