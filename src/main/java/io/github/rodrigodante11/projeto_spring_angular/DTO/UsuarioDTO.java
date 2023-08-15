d4448e3d143bd6c54a04425c8002deb1607c30b1
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2023-08-15 08:15:24 -0300

    adicionando mesagens de erro por messages.properties

--- src/main/java/io/github/rodrigodante11/projeto_spring_angular/DTO/UsuarioDTO.java
+++ src/main/java/io/github/rodrigodante11/projeto_spring_angular/DTO/UsuarioDTO.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 
 import javax.persistence.Column;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Data
@@ -12,9 +13,17 @@ import java.time.LocalDate;
 public class UsuarioDTO {
 
     private Long id;
+
+    @NotNull(message = "{ campo.nome.obrigatorio }")
     private String nome;
+
+    @NotNull(message = "{campo.email.obrigatorio}")
     private String email;
+
     private String telefone;
+
+    @NotNull(message = "{campo.senha.obrigatorio}")
     private String senha;
+
     private boolean isAdmin;
 }
--- src/main/java/io/github/rodrigodante11/projeto_spring_angular/controller/UsuarioController.java
+++ src/main/java/io/github/rodrigodante11/projeto_spring_angular/controller/UsuarioController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -25,7 +26,6 @@ public class UsuarioController {
         try{
             Usuario usuario = Converter.usuarioConverter(usuarioDTO);
 
-            usuario = usuarioService.salvar(usuario);
 
             return new ResponseEntity<>(usuario,HttpStatus.CREATED);
         }catch (ErroUsuarioException e){
@@ -33,9 +33,8 @@ public class UsuarioController {
         }
     }
 
-
     @PostMapping("/autenticar")
-    public ResponseEntity autenticarUsuario(@RequestBody UsuarioDTO usuarioDTO){
+    public ResponseEntity autenticarUsuario(@RequestBody @Valid UsuarioDTO usuarioDTO){
 
         try{
             Usuario usuarioAutenticado = usuarioService.autenticar(usuarioDTO.getEmail(), usuarioDTO.getSenha());
